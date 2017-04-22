23b173647f1ce64c1674a72495d9187da26235b6
Francisco Nievas <frannievas@gmail.com>
2017-04-22 18:14:27 -0300

    Added ej2.py

---
+++ tp4/ej2.py
@@ -0,0 +1,30 @@
+from random import random
+from math import exp
+
+N = 10000
+ITER = 100
+
+def udiscrete(m, k):
+    """
+    Uniform discrete variable in interval [m, k]
+    """
+    u = random()
+    return int(u*(k-m+1))+m
+
+
+def experiment():
+    s = [ exp(udiscrete(0,N) / N) for x in range(ITER)]
+    return (sum(s) / ITER ) * N
+
+
+def real_value():
+    s = [ exp(x/N) for x in range(1, N+1)]
+    return sum(s)
+
+
+if __name__ == '__main__':
+    s = experiment()
+    r = real_value()
+    print("Estimate: {}".format(s))
+    print("Real value: {}".format(r))
+    print("Diference: {}".format(abs(r-s)))
