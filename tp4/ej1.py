0e0496dcbd9acf7fcc3fb8806ea251b90f69fa4e
Francisco Nievas <frannievas@gmail.com>
2017-04-22 17:20:09 -0300

    flake8 ej1.py

--- tp4/ej1.py
+++ tp4/ej1.py
@@ -12,17 +12,19 @@ def udiscrete(m, k):
     u = random()
     return int(u*(k-m+1))+m
 
+
 def permutation(x):
     """
     Random permutation
     """
-    for i in range(N-1,0,-1):
+    for i in range(N-1, 0, -1):
         # Uniform in [0,i]
         index = udiscrete(0, i)
         tmp = x[index]
         x[index] = x[i]
         x[i] = tmp
     return x
+
 
 def experiment():
     """
@@ -34,21 +36,18 @@ def experiment():
 
     return sum(success)
 
+
 if __name__ == '__main__':
 
     # Estimate the mean of the succes experiments
-    success = []
-    for i in range(ITER):
-        success.append(experiment())
+    success = [experiment() for x in range(ITER)]
     mean = sum(success) / ITER
     print("Mean: {}".format(mean))
 
     # Estimate the mean of the succes experiments
-    success = []
-    for i in range(ITER):
-        success.append(experiment() ** 2)
+    success = [experiment() ** 2 for x in range(ITER)]
     mean2 = sum(success) / ITER
     print("Mean2: {}".format(mean2))
 
-    varianza = mean2 -  (mean ** 2)
-    print("varianza: {}".format(varianza))
+    variance = mean2 - (mean ** 2)
+    print("variance: {}".format(variance))
