3cac53d0e2723904ec8fd2395fbe64bd81370c5d
Francisco Nievas <frannievas@gmail.com>
2017-04-27 11:31:54 -0300

    First method in exercise 4 finished

--- tp4/ej4.py
+++ tp4/ej4.py
@@ -12,34 +12,85 @@ def Poisson_acum(l, i):
 def Poisson(l):
     i = int(l)
     # Calcular F(I) usando la definición recursiva de p_i
-    f = Poisson_acum(l, i)
+    p = Poisson_acum(l, i)
+    f = p
     u = random()
     if u >= f:
         # generar X haciendo búsqueda ascendente
         while u >= f:
             i += 1
-            f *= l / i
+            p *= l / i
+            f += p
         i -= 1
     else:
         # generar X haciendo búsqueda descendente.
         while u < f:
             i -= 1
-            f *= (l / i)
+            p *= i / l
+            f += p
         i += 1
 
     return i
 
-def experiment(l, k):
+
+def Poisson_naive(l, k):
     """
     Desarrollar dos métodos para generar una variable aleatoria X.
     """
     u = random()
     i = 0
-    f = Poisson(l) / sum([Poisson(l) for i in range(k)])
-    while u > f:
+    p = exp(-l)
+    f = p
+    while u >= f:
         i += 1
-        f *= l / i
-    if f == 0:
-        return 0
-   else:
-        return i
+        p *= l / i
+        f += p
+
+    return i
+
+
+def experiment_naive(l, k):
+    """
+    Desarrollar dos métodos para generar una variable aleatoria X.
+    """
+    u = random()
+    i = 0
+    denom = sum([ Poisson_acum(l,x) for x in range(k+1)])
+    p = exp(-l) / denom
+    f = p
+    while u >= f:
+        i += 1
+        p *= l / i
+        f += p
+
+    return i
+
+
+def experiment(l, k):
+    i = int(l)
+    denom = sum([ Poisson_acum(l,x) for x in range(k+1)])
+    # Calcular F(I) usando la definición recursiva de p_i
+    p = Poisson_acum(l, i) / denom
+    u = random()
+    f = p
+    if u >= f:
+        # generar X haciendo búsqueda ascendente
+        while u >= f:
+            i += 1
+            p *= l / i
+            f += p
+        i -= 1
+    else:
+        # generar X haciendo búsqueda descendente.
+        while u < f:
+            i -= 1
+            p *= (i / l)
+            f -= p
+        i += 1
+
+    return i
+
+if __name__ == '__main__':
+    l = 2
+    k = 10
+    print(experiment_naive(l, k))
