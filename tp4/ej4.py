3a32da6c14c2115025adc60251e43fb81a88f1b8
Francisco Nievas <frannievas@gmail.com>
2017-05-05 17:11:17 -0300

    BUGFIX Poisson

--- tp4/ej4.py
+++ tp4/ej4.py
@@ -2,12 +2,20 @@ from random import random
 from math import exp
 
 
-def Poisson_acum(l, i):
+def Poisson_pi(l, i):
+    """
+    Calcula Pi
+    """
     prob = exp(-l)
     for j in range(0, i):
         prob *= (l / (j + 1))
     return prob
 
+def Poisson_acum(l, k):
+    """
+    Calcula acumulada F(k) = P0 + P1 + ... + Pk
+    """
+    return sum([Poisson_pi(l, i) for i in range(k+1)])
 
 def Poisson(l):
     i = int(l)
@@ -21,13 +29,13 @@ def Poisson(l):
             i += 1
             p *= l / i
             f += p
-        i -= 1
+
     else:
         # generar X haciendo búsqueda descendente.
         while u < f:
-            i -= 1
             p *= i / l
-            f += p
+            i -= 1
+            f -= p
         i += 1
 
     return i
@@ -55,7 +63,7 @@ def experiment_naive(l, k):
     """
     u = random()
     i = 0
-    denom = sum([ Poisson_acum(l,x) for x in range(k+1)])
+    denom = Poisson_acum(l, k)
     p = exp(-l) / denom
     f = p
     while u >= f:
@@ -64,6 +72,14 @@ def experiment_naive(l, k):
         f += p
 
     return i
+
+def rejection(l):
+    denom = Poisson_acum(l, k)
+    u, Y = random(), random()
+    while u >= Poisson_acum(l,Y) / denom:
+        u, Y = random(), random()
+
+    return Y
 
 
 def experiment(l, k):
@@ -91,6 +107,6 @@ def experiment(l, k):
     return i
 
 if __name__ == '__main__':
-    l = 2
+    l = 20
     k = 10
-    print(experiment_naive(l, k))
+    print(Poisson(l))
