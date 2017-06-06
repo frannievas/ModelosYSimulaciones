f9b841639936ace1515298377f26cc69cb1da5ac
Francisco Nievas <frannievas@gmail.com>
2017-06-06 12:23:07 -0300

    Added Tp7

---
+++ tp7/functions.py
@@ -0,0 +1,102 @@
+from random import random
+from math import log2, log, sqrt
+
+def udiscrete(m, k):
+    """
+    Uniform discrete variable in interval [m, k]
+    """
+    u = random()
+    return int(u*(k-m+1))+m
+
+
+def interval(X, c, S, n):
+    """
+    Calcula los intervalos de confianza para los parametros recibidos
+
+    :param X: Media muestral
+    :param C: Constanste Zalfa
+    :param S: Varianza muestral
+    :param n: Largo de la muestra
+    """
+    return (X - c * (S/sqrt(n)), X + c * (S/sqrt(n)))
+
+
+def exponential(lamda):
+    """
+    Genera una va Exponencial, a partir de su único parámentro lamda,
+    utilizando el método de la transformada inversa.
+    """
+    return(- log(random()) / lamda)
+
+
+def normal(mu, sigma):
+    """
+    Esta función devuelve una va normal, utilizando el método de Aceptación y
+    Rechazo mediante 2 va exponenciales.
+    """
+    Y1, Y2 = exponential(1), exponential(1)
+
+    while Y2 < (Y1 - 1) ** 2 / 2:
+        Y1, Y2 = exponential(1), exponential(1)
+
+    if random() < 0.5:
+        return(Y1 * sigma + mu)
+    else:
+        return(-Y1 * sigma + mu)
+
+
+def normal_estandar():
+    while True:
+        Y1 = -log2(random())
+        Y2 = -log2(random())
+        if Y2 >= (Y1 - 1) ** 2:
+            return Y1
+
+
+def prom_sample(Xi):
+    """Calcula el promedio de los Xi"""
+    return sum(Xi) / len(Xi)
+
+
+def avg_rec(X, n, Xi):
+    """
+    :param X: Promedio con n-1 valores
+    :param n:
+    :param Xn: Valor n-esimo
+    """
+    return X + (Xi[n-1] - X) / (n + 1)
+
+
+def var(Xi, X):
+    result = [(i - X) ** 2 for i in Xi]
+    return sum(result) / (len(Xi) - 1)
+
+
+def var_sample(Xi):
+    """
+    Calcula la varianza muestral
+    """
+    S2 = 0
+    X = Xi[0]
+    for i in range(len(Xi)):
+        Xnew = avg_rec(X, i+1, Xi)
+        S2 = (1 - 1 / (i + 1)) * S2 + (i + 2) * (Xnew - X) ** 2
+        X = Xnew
+    return S2
+
+
+def empiric(x, values):
+    """
+    Funcion empirica
+    :param x: valor en el que se desea evaluar la funcion
+    :param values: valores que toma la funcion empirica
+    """
+    n = len(values)
+    if x < values[0]:
+        return 0
+
+    for i in range(len(values) - 1):
+        if (x >= values[i]) and (x < values[i+1]):
+            return (i+1) / n
+
+    return 1
